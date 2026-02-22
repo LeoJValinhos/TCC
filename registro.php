96e5923adca427325d92200511b35bf522ba2031
Leo <leoj.valinhos@gmail.com>
2026-02-22 17:51:05 -0300

    testeleleco sla

--- registro.php
+++ registro.php
@@ -3,11 +3,16 @@ require_once "conexao.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = trim($_POST["nome"]);
+    $sobrenome = trim($_POST["sobrenome"]);
+    $email = trim($_POST["email"]);
+    $datanascimento = trim($_POST["datanascimento"]);
+    $cpf = trim($_POST["cpf"]);
+    $celular = trim($_POST["celular"]);
     $tipo = trim($_POST["tipo"]);
 
-    if (!empty($nome) && !empty($tipo)) {
-        $stmt = $conn->prepare("INSERT INTO funcionarios (NomeFuncionario, TipoCadastro) VALUES (?, ?)");
-        $stmt->bind_param("ss", $nome, $tipo);
+    if (!empty($nome) && !empty($sobrenome)  && !empty($email) && !empty($datanascimento)  && !empty($cpf)  && !empty($celular) && !empty($tipo)) {
+        $stmt = $conn->prepare("INSERT INTO cadastros (nome,sobrenome,email,datanasc,cpf,celular,tipoCadastro) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssss", $nome, $sobrenome, $email, $datanascimento, $cpf, $celular, $tipo);
 
         if ($stmt->execute()) {
             // Redireciona na hora com um parâmetro de sucesso na URL
