e022bdf489d64c0bf34bfb3f16491cb7e1ecf958
ItzMeSales <vinicius.salesbb@gmail.com>
2026-03-04 19:43:32 -0300

    Update databasetcc.sql

--- databasetcc.sql
+++ databasetcc.sql
@@ -1,136 +1,86 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 22-Fev-2026 às 20:17
--- Versão do servidor: 5.7.36
--- versão do PHP: 8.1.3
+-- phpMyAdmin SQL Dump CORRIGIDO
+-- Banco: databasetcc
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
+SET FOREIGN_KEY_CHECKS = 0;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `databasetcc`
---
+CREATE DATABASE IF NOT EXISTS `databasetcc`;
+USE `databasetcc`;
 
 -- --------------------------------------------------------
-
---
--- Estrutura da tabela `cadastros`
---
+-- TABELA: cadastros
+-- --------------------------------------------------------
 
 CREATE TABLE `cadastros` (
-  `idCadastro` int(11) NOT NULL,
-  `nome` varchar(30) NOT NULL,
-  `sobrenome` varchar(60) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `datanasc` date NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `celular` varchar(11) NOT NULL,
-  `tipocadastro` enum('EMPRESA/ADM','INDEPENDENTE/USUARIO') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `idCadastro` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(30) NOT NULL,
+  `sobrenome` VARCHAR(60) NOT NULL,
+  `email` VARCHAR(60) NOT NULL,
+  `datanasc` DATE NOT NULL,
+  `cpf` VARCHAR(11) NOT NULL,
+  `celular` VARCHAR(11) NOT NULL,
+  `tipocadastro` ENUM('EMPRESA/ADM','INDEPENDENTE/USUARIO') NOT NULL,
+  PRIMARY KEY (`idCadastro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
+-- TABELA: funcionarios (CRIADA PARA RESOLVER O ERRO)
+-- --------------------------------------------------------
 
---
--- Estrutura da tabela `produtos`
---
+CREATE TABLE `funcionarios` (
+  `IDFuncionario` INT(11) NOT NULL AUTO_INCREMENT,
+  `nomeFuncionario` VARCHAR(100) NOT NULL,
+  `emailFuncionario` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`IDFuncionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+-- TABELA: produtos
+-- --------------------------------------------------------
 
 CREATE TABLE `produtos` (
-  `idProduto` int(11) NOT NULL,
-  `NomeProduto` varchar(100) NOT NULL,
-  `MarcaProduto` varchar(100) NOT NULL,
-  `Descricao` varchar(255) DEFAULT NULL,
-  `Criadopor` int(11) NOT NULL,
-  `criadoem` datetime DEFAULT CURRENT_TIMESTAMP
+  `idProduto` INT(11) NOT NULL AUTO_INCREMENT,
+  `NomeProduto` VARCHAR(100) NOT NULL,
+  `MarcaProduto` VARCHAR(100) NOT NULL,
+  `Descricao` VARCHAR(255) DEFAULT NULL,
+  `Criadopor` INT(11) NOT NULL,
+  `criadoem` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idProduto`),
+  UNIQUE KEY `NomeProduto` (`NomeProduto`),
+  KEY `Criadopor` (`Criadopor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
---
--- Estrutura da tabela `produtoslotes`
---
+-- TABELA: produtoslotes
+-- --------------------------------------------------------
 
 CREATE TABLE `produtoslotes` (
-  `idlote` int(11) NOT NULL,
-  `idproduto` int(11) NOT NULL,
-  `quantidade` int(11) NOT NULL,
-  `validade` date DEFAULT NULL,
-  `criado_em` datetime DEFAULT CURRENT_TIMESTAMP
+  `idlote` INT(11) NOT NULL AUTO_INCREMENT,
+  `idproduto` INT(11) NOT NULL,
+  `quantidade` INT(11) NOT NULL,
+  `validade` DATE DEFAULT NULL,
+  `criado_em` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idlote`),
+  KEY `idproduto` (`idproduto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Índices para tabelas despejadas
---
+-- --------------------------------------------------------
+-- FOREIGN KEYS
+-- --------------------------------------------------------
 
---
--- Índices para tabela `cadastros`
---
-ALTER TABLE `cadastros`
-  ADD PRIMARY KEY (`idCadastro`);
-
---
--- Índices para tabela `produtos`
---
 ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`idProduto`),
-  ADD UNIQUE KEY `NomeProduto` (`NomeProduto`),
-  ADD KEY `Criadopor` (`Criadopor`);
+  ADD CONSTRAINT `produtos_ibfk_1`
+  FOREIGN KEY (`Criadopor`)
+  REFERENCES `funcionarios` (`IDFuncionario`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
---
--- Índices para tabela `produtoslotes`
---
 ALTER TABLE `produtoslotes`
-  ADD PRIMARY KEY (`idlote`),
-  ADD KEY `idproduto` (`idproduto`);
+  ADD CONSTRAINT `produtoslotes_ibfk_1`
+  FOREIGN KEY (`idproduto`)
+  REFERENCES `produtos` (`idProduto`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `cadastros`
---
-ALTER TABLE `cadastros`
-  MODIFY `idCadastro` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `produtos`
---
-ALTER TABLE `produtos`
-  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `produtoslotes`
---
-ALTER TABLE `produtoslotes`
-  MODIFY `idlote` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `produtos`
---
-ALTER TABLE `produtos`
-  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`Criadopor`) REFERENCES `funcionarios` (`IDFuncionario`);
-
---
--- Limitadores para a tabela `produtoslotes`
---
-ALTER TABLE `produtoslotes`
-  ADD CONSTRAINT `produtoslotes_ibfk_1` FOREIGN KEY (`idproduto`) REFERENCES `produtos` (`idProduto`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
