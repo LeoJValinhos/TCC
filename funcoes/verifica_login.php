5548f5a80f1351bad002036171cf2bd005c9a027
vinicius <vinicius.salesbb@gmail.com>
2026-05-29 21:38:23 -0300

    wergfhrt342

--- funcoes/verifica_login.php
+++ funcoes/verifica_login.php
@@ -1,8 +1,15 @@
 <?php
+
 session_start();
 
-if(!isset($_SESSION['nome']) || !isset($_SESSION['idCadastro'])){
+if(
+    !isset($_SESSION['nome']) ||
+    !isset($_SESSION['idCadastro'])
+){
+
     header("Location: ../registro_login/login.html");
     exit();
+
 }
+
 ?>
