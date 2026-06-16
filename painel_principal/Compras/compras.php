a797d55c6217c82379806955bb7a92cc1d2953c4
vinicius <vinicius.salesbb@gmail.com>
2026-06-15 21:50:57 -0300

    .lkjhgftyu8iyg

--- painel_principal/Compras/compras.php
+++ painel_principal/Compras/compras.php
@@ -19,6 +19,61 @@ require_once("produtos_simulado.php");
 
 <body>
 
+
+ <!-- TOPO -->
+    <header class="topbar">
+
+        <div class="top-left">
+
+            <img src="../../Imagens/carrinho2.png"
+                 width="70"
+                 height="70"
+                 alt="Logo Carrinho">
+
+            <h1>INVEX</h1>
+
+        </div>
+
+    </header>
+
+<div class="layout">
+
+    <!-- SIDEBAR -->
+        <aside class="sidebar">
+
+            <nav>
+
+                <a href="../painel_principal.php">
+                    🏠 Home
+                </a>
+
+                <a href="../cadastro_produtos/cad_list_prods.php">
+                    📦 Produtos
+                </a>
+
+                 <a href="../compras/compras.php">
+                    🛒​ Compras
+                </a>
+
+                <a href="../relatorios/relatorios.html">
+                    📊 Relatórios
+                </a>
+
+                <a>
+                    ⚙️ Configurações
+                </a>
+
+            </nav>
+
+            <a href="../../index.html"
+               class="logout">
+
+                🚪 Sair
+
+            </a>
+
+        </aside>
+
 <h1>
     Compras coletivas
 </h1>
